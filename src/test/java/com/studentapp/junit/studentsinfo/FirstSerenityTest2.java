991413bb3ef298343f098b761025f8240b75f3e7
Renu Paturi <renupaturi@gmail.com>
2019-10-02 11:25:23 -0500

    Added a new testcase

---
+++ src/test/java/com/studentapp/junit/studentsinfo/FirstSerenityTest2.java
@@ -0,0 +1,29 @@
+package com.studentapp.junit.studentsinfo;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.restassured.RestAssured;
+import net.serenitybdd.junit.runners.SerenityRunner;
+
+@RunWith(SerenityRunner.class)
+public class FirstSerenityTest2 {
+
+	@BeforeClass
+	public static void init()
+	{
+		RestAssured.baseURI="http://localhost:8085/student";
+	}
+	@Test
+	public void getAllStudents()
+	{
+		RestAssured.given()
+		.when()
+		.get("/list")
+		.then()
+		.log()
+		.all();
+		
+	}
+}
